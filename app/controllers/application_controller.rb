321825854bcb2b143c9d902270005f9972cdbe52
Chen Lin <chen.lin@rea-group.com>
2021-03-31 10:23:36 +0800

    master change save to post request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
 
   def greet
     render json: { message: 'hello chris' }
   end
 
   def createAccount
-    newAccount = Account.new(:name => 'linchen')
+    newAccount = Account.new(:name => params[:name])
     newAccount.save
     render json: {message: 'save success'}
   end
